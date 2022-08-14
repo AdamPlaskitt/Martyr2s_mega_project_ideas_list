81832ca8d5f23e7093dfb395276d06ebf1bf9b6d
Adam Plaskitt <adam.plaskitt1@gmail.com>
2022-08-14 17:42:12 +0100

    Use file scoped namespace

--- CSharp/RandomProjects/Numbers.Tests/FibonacciTests.cs
+++ CSharp/RandomProjects/Numbers.Tests/FibonacciTests.cs
@@ -1,25 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// <copyright file="FibonacciTests.cs" company="Adam Plaskitt">
+// Copyright (c) Adam Plaskitt. All rights reserved.
+// </copyright>
 
-namespace Numbers.Tests
+namespace Numbers.Tests;
+
+public class FibonacciTests
 {
-    public class FibonacciTests
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(1, 1)]
+    [InlineData(2, 1)]
+    [InlineData(3, 2)]
+    [InlineData(5, 5)]
+    [InlineData(11, 89)]
+    public void Model_run_with_valid_returns_nth_digit(int number, int answer)
     {
-        [Theory]
-        [InlineData(0, 0)]
-        [InlineData(1, 1)]
-        [InlineData(2, 1)]
-        [InlineData(3, 2)]
-        [InlineData(5, 5)]
-        [InlineData(11, 89)]
-        public void Model_run_with_valid_returns_nth_digit(int number, int answer)
-        {
-            int actualResult = Fibonacci.GetElementByIndex(number);
+        int actualResult = Fibonacci.GetElementByIndex(number);
 
-            actualResult.Should().Be(answer);
-        }
+        actualResult.Should().Be(answer);
     }
 }
--- CSharp/RandomProjects/Numbers/Fibonacci.cs
+++ CSharp/RandomProjects/Numbers/Fibonacci.cs
@@ -2,40 +2,39 @@
 // Copyright (c) Adam Plaskitt. All rights reserved.
 // </copyright>
 
-namespace Numbers
+namespace Numbers;
+
+/// <summary>
+/// Represents actions around the Fibonacci sequence.
+/// </summary>
+public class Fibonacci
 {
     /// <summary>
-    /// Represents actions around the Fibonacci sequence.
+    /// Calculates the Nth element in the Fibonacci sequence.
     /// </summary>
-    public class Fibonacci
+    /// <param name="number">The elemt to get.</param>
+    /// <returns>The Nth element of the Fibonacci sequence.</returns>
+    public static int GetElementByIndex(int number)
     {
-        /// <summary>
-        /// Calculates the Nth element in the Fibonacci sequence.
-        /// </summary>
-        /// <param name="number">The elemt to get.</param>
-        /// <returns>The Nth element of the Fibonacci sequence.</returns>
-        public static int GetElementByIndex(int number)
+        if (number == 0)
         {
-            if (number == 0)
-            {
-                return 0;
-            }
-            else if (number == 1)
-            {
-                return 1;
-            }
-
-            int oldElement = 0;
-            int newElement = 1;
-
-            for (int i = 1; i < number; i++)
-            {
-                int tmp = oldElement;
-                oldElement = newElement;
-                newElement = tmp + oldElement;
-            }
-
-            return newElement;
+            return 0;
         }
+        else if (number == 1)
+        {
+            return 1;
+        }
+
+        int oldElement = 0;
+        int newElement = 1;
+
+        for (int i = 1; i < number; i++)
+        {
+            int tmp = oldElement;
+            oldElement = newElement;
+            newElement = tmp + oldElement;
+        }
+
+        return newElement;
     }
 }
